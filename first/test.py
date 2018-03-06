eff76f2916a4b78da32e4ccfe10c483cb050d4af
backonhighway <pocketsuteado@gmail.com>
2018-03-06 23:40:04 +0900

    done for day

---
+++ first/test.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import pocket_lgb
+from sklearn import model_selection
+
+train = pd.read_csv('../input/train_sample.csv')
+# test = pd.read_csv('../input/test.csv')
+
+
+print(train.head())
+
+print(train.describe())
+
+use_col = ["ip", "app", "device", "os", "channel", "is_attributed"]
+train = train[use_col]
+train_y = train["is_attributed"]
+train_x = train.drop("is_attributed", axis=1)
+
+X_train, X_valid, y_train, y_valid = model_selection.train_test_split(train_x, train_y, test_size=0.2, random_state=99)
+
+
+lgb = pocket_lgb.GoldenLgb()
+lgb.do_train_sk(X_train, X_valid, y_train, y_valid)
+
+print("end")
