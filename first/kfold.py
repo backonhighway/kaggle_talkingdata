dbc0e2149f430797b8e6d3cde898d916e12d5496
backonhighway <pocketsuteado@gmail.com>
2018-03-14 23:44:47 +0900

    done for day

--- first/kfold.py
+++ first/kfold.py
@@ -2,59 +2,34 @@ import pandas as pd
 import numpy as np
 import pocket_lgb
 from sklearn import model_selection
+import feature_engineerer
+import holdout_validator
 
-input_df = pd.read_csv('../input/train_first_1000k.csv')
-# test = pd.read_csv('../input/test.csv')
+input_df = pd.read_csv('../input/train_day3_first_1000k.csv')
 
+feature_engineerer.do_feature_engineering(input_df)
+print(input_df.describe())
 
-#print(train.tail())
-#print(train.head())
-#print(train.describe())
-#print(train.apply(pd.Series.nunique))
-
-
-def add_count(df):
-    df["ip_count"] = df.groupby("ip")["is_attributed"].transform('count')
-    df["device_count"] = df.groupby("device")["is_attributed"].transform('count')
-    df["os_count"] = df.groupby("os")["is_attributed"].transform('count')
-    df["user_count"] = df.groupby(["ip", "device", "os"])["is_attributed"].transform('count')
-    df["app_count"] = df.groupby("app")["is_attributed"].transform('count')
-    df["channel_count"] = df.groupby("channel")["is_attributed"].transform('count')
-
-
-def target_encode(df):
-    df["ip_target"] = df.groupby("ip")["is_attributed"].transform(np.mean)
-    df["device_target"] = df.groupby("device")["is_attributed"].transform(np.mean)
-    df["os_target"] = df.groupby("os")["is_attributed"].transform(np.mean)
-    df["user_target"] = df.groupby(["ip", "device", "os"])["is_attributed"].transform(np.mean)
-    df["app_target"] = df.groupby("app")["is_attributed"].transform(np.mean)
-    df["channel_target"] = df.groupby("channel")["is_attributed"].transform(np.mean)
-
-
-use_col = ["ip", "app", "device", "os", "channel", "is_attributed"]
-drop_col = ["click_time", "attributed_time"]
-input_df = input_df.drop(drop_col, axis=1)
-add_count(input_df)
-# train_y = input_df["is_attributed"]
-# train_x = input_df.drop("is_attributed", axis=1)
-
-skf = model_selection.KFold(n_splits=5)
-# for train_index, test_index in skf.split(input_df, input_df["is_attributed"]):
-#    train = input_df[train_index]
-#    test = input_df[test_index]
+split_number = 5
+skf = model_selection.KFold(n_splits=split_number)
+lgb = pocket_lgb.GoldenLgb()
+first_model = None
+total_score = 0
 for train_index, test_index in skf.split(input_df):
     train_np = input_df.iloc[train_index]
     test_np = input_df.iloc[test_index]
     train_df = pd.DataFrame(train_np)
     test_df = pd.DataFrame(test_np)
 
-    #t_t = input_df.iloc(train_index)
-    #te_t = input_df.iloc(test_index)
+    model = lgb.do_train(train_df, test_df)
+    score = model.best_score["valid_0"]["auc"]
+    total_score += score
 
-    lgb = pocket_lgb.GoldenLgb()
-    lgb.do_train(train_df, test_df)
-    #lgb.do_train_sk(X_train, X_test, y_train, y_test)
+    if first_model is None:
+        first_model = model
 
+print("average score= ", total_score / split_number)
 
-
-print("end")
+lgb.show_feature_importance(first_model)
+validator = holdout_validator.HoldoutValidator(first_model)
+validator.validate()
--- first/submit.py
+++ first/submit.py
@@ -4,6 +4,7 @@ import pocket_lgb
 from sklearn import model_selection
 import  feature_engineerer
 import gc
+import time
 
 train = pd.read_csv('../input/train_day3_first_10000k.csv')
 
@@ -25,20 +26,23 @@ lgb.show_feature_importance(model)
 del train
 gc.collect()
 
-
-test = pd.read_csv('../input/test_hour4.csv')
+s_start_time = time.time()
+test = pd.read_csv('../input/test.csv')
 feature_engineerer.do_feature_engineering(test)
 y_pred = model.predict(test)
 submission = pd.DataFrame({"click_id": test["click_id"], "is_attributed": y_pred})
 print(submission.describe())
 print("done prediction")
-# submission.to_csv("../output/submission.csv", float_format='%.6f', index=False)
+submission.to_csv("../output/submission.csv", float_format='%.6f', index=False)
 
 
-sample = pd.read_csv('../input/sample_submission.csv', usecols=["click_id"])
-output = sample.merge(submission, on="click_id", how="left")
-output["is_attributed"] = output["is_attributed"].fillna(1)
-print(output.describe())
-
-output.to_csv("../output/only_public.csv", float_format='%.6f', index=False)
+# sample = pd.read_csv('../input/sample_submission.csv', usecols=["click_id"])
+# output = sample.merge(submission, on="click_id", how="left")
+# output["is_attributed"] = output["is_attributed"].fillna(1)
+# print(output.describe())
+#
+# output.to_csv("../output/only_public.csv", float_format='%.6f', index=False)
+#
+s_end_time = time.time()
+print("submission in ", s_end_time - s_start_time)
 
