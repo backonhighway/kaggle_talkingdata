064a797040a92b7959fd3b2e0fb653a07280e74c
backonhighway <pocketsuteado@gmail.com>
2018-03-30 20:19:44 +0900

    attr check

--- common/pocket_timer.py
+++ common/pocket_timer.py
@@ -3,13 +3,12 @@ import time
 class GoldenTimer:
     def __init__(self, logger_arg=None):
         self.start_time = time.time()
-        self.logger = None
         if logger_arg is not None:
             self.logger = logger_arg
 
     def time(self, print_str):
         duration = time.time() - self.start_time
         print(print_str, duration)
-        if self.logger is not None:
+        if hasattr(self, "logger"):
             self.logger.info(print_str, duration)
         self.start_time = time.time()
