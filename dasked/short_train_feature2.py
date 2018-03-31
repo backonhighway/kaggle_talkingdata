41ed0d27439aa994e559c629f30a863ac22baf32
backonhighway <pocketsuteado@gmail.com>
2018-04-01 00:44:03 +0900

    okpk

---
+++ dasked/short_train_feature2.py
@@ -0,0 +1,15 @@
+import os, sys
+ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../'))
+sys.path.append(ROOT)
+APP_ROOT = os.path.join(ROOT, "talkingdata")
+INPUT_DIR = os.path.join(APP_ROOT, "input")
+OUTPUT_DIR = os.path.join(APP_ROOT, "output")
+
+import pandas as pd
+import numpy as np
+from talkingdata.dasked import short_feature_module
+
+TRAIN_FILE = os.path.join(INPUT_DIR, "train_day8.csv")
+OUTPUT_FILE = os.path.join(OUTPUT_DIR, "short_train_day8.csv")
+
+short_feature_module.make_file(TRAIN_FILE, OUTPUT_FILE)
