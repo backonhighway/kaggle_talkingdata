0b436624176daaf2239c8eb8f6c45cd8e4f8cc5c
backonhighway <pocketsuteado@gmail.com>
2018-03-31 09:52:32 +0900

    try short

--- common/feature_engineerer.py
+++ common/feature_engineerer.py
@@ -110,7 +110,24 @@ def get_necessary_col():
 
 
 def get_test_col():
-    use_col = [
+    return get_short_col()
+
+def get_short_col():
+    short_col = [
+        'app', 'device', 'os', 'channel',
+        'hour',
+        'group_i_count', 'group_idoa_count', 'group_ioac_count',
+        'group_i_prev_click_time', 'group_i_next_click_time',
+        # 'group_ido_prev_click_time', 'group_ido_next_click_time',
+        'group_i_nunique_os', 'group_i_nunique_app', 'group_i_nunique_channel',
+        'group_idoct_sum'
+        #'group_ict_max', 'group_ict_std', 'group_ict_mean', 'group_ict_sum',
+        #'group_idoct_max', 'group_idoct_std', 'group_idoct_mean', 'group_idoct_sum'
+    ]
+    return short_col
+
+def get_full_col():
+    full_col = [
         'app', 'device', 'os', 'channel',
         'hour', 'app_count', 'os_count',
         'idoa_is_last_try', #"telling_ip",
@@ -127,4 +144,4 @@ def get_test_col():
         'group_ict_max', 'group_ict_std', 'group_ict_mean', 'group_ict_sum',
         'group_idoct_max', 'group_idoct_std', 'group_idoct_mean', 'group_idoct_sum'
     ]
-    return use_col
+    return full_col
