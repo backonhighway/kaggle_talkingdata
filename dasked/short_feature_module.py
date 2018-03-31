40884f7cc689e45d4bdda7961c8b6ddacbacaf31
backonhighway <pocketsuteado@gmail.com>
2018-03-31 23:49:41 +0900

    dask is slow

--- dasked/short_feature_module.py
+++ dasked/short_feature_module.py
@@ -11,7 +11,7 @@ from dask import dataframe as dd
 from talkingdata.common import csv_loader
 
 
-def basic(df: dd.DataFrame):
+def basic(df: pd.DataFrame):
     #df['day'] = df.click_time.str[8:10].astype(int)
     df['hour'] = df.click_time.str[11:13].astype(int)
     df["click_time"] = dd.to_datetime(df["click_time"])
@@ -26,7 +26,7 @@ def do_pandas(df: pd.DataFrame):
     df["idoa_is_last_try"] = np.where(df["idoa_is_last_try"].isnull(), 1, 0)
 
 
-def do_grouping(df: dd.DataFrame):
+def do_grouping(df: pd.DataFrame):
     group_list = {
         "group_i": ["ip"],
         "group_idoa": ["ip", "app", "os", "device"],
@@ -59,7 +59,7 @@ def do_grouping(df: dd.DataFrame):
     #    get_interval_click_time(df, name, grouping)
 
 
-def get_interval_click_time(df: dd.DataFrame, name: str, grouping:list):
+def get_interval_click_time(df: pd.DataFrame, name: str, grouping:list):
     grouper = df.groupby(grouping)
     pct_col = name + "_prev_click_time"
     nct_col = name + "_next_click_time"
@@ -72,36 +72,34 @@ def get_interval_click_time(df: dd.DataFrame, name: str, grouping:list):
     df[nct_col] = df[nct_col].dt.total_seconds()
 
 
-def get_short_stats(df: dd.DataFrame, name: str, grouping:list):
+def get_short_stats(df: pd.DataFrame, name: str, grouping:list):
     pct_col = name + "_prev_click_time"
     sum_col = name + "ct_sum"
     df[sum_col] = df.groupby(grouping)[pct_col].transform("sum")
 
 
-def get_counts(df: dd.DataFrame, name: str, grouping:list):
+def get_counts(df: pd.DataFrame, name: str, grouping:list):
     grouper = df.groupby(grouping)
     cnt_col = name + "_count"
     df[cnt_col] = grouper["device"].transform("count")
 
 
-def get_nunique(df: dd.DataFrame, name: str, grouping:list, nunique_col:str):
+def get_nunique(df: pd.DataFrame, name: str, grouping:list, nunique_col:str):
     grouper = df.groupby(grouping)
     uni_col = name + "_nunique"+ "_" + nunique_col
     df[uni_col] = grouper[nunique_col].transform("nunique")
 
 
-def do_it_all(df: dd.DataFrame):
+def do_it_all(df: pd.DataFrame):
     basic(df)
     print("done basic features")
     do_grouping(df)
     print("done grouping features")
-    df = df.compute()
-    do_pandas(df)
 
 
 def make_file(input_file, output_file):
     dtypes = csv_loader.get_dtypes()
-    input_df = dd.read_csv(input_file, dtype=dtypes)
+    input_df = dd.read_csv(input_file, dtype=dtypes).compute()
     do_it_all(input_df)
 
     input_df.to_csv(output_file, float_format='%.6f', index=False)
