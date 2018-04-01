59a9a3f1079ce70da358fbbd6e3c74e57f90b3e8
backonhighway <pocketsuteado@gmail.com>
2018-04-01 17:36:02 +0900

    eda

---
+++ eda/show_rank.py
@@ -0,0 +1,26 @@
+import os, sys
+ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../'))
+sys.path.append(ROOT)
+APP_ROOT = os.path.join(ROOT, "talkingdata")
+INPUT_DIR = os.path.join(APP_ROOT, "input")
+TRAIN_DATA = os.path.join(INPUT_DIR, "train_day9.csv")
+
+import pandas as pd
+import numpy as np
+import gc
+from sklearn import model_selection
+from dask import dataframe as dd
+from talkingdata.common import csv_loader, feature_engineerer, holdout_validator, pocket_lgb, pocket_timer, pocket_logger
+
+logger = pocket_logger.get_my_logger()
+timer = pocket_timer.GoldenTimer(logger)
+use_col = feature_engineerer.get_necessary_col()
+dtypes = csv_loader.get_featured_dtypes()
+train = dd.read_csv(TRAIN_DATA, dtype=dtypes, usecols=use_col).compute()
+#print(train.info())
+timer.time("load csv in ")
+
+merge_col = ["ip", "app", "device", "os", "channel", "click_time"]
+train["rank"] = train.groupby(merge_col).rank().astype("int")
+
+print(train.groupby("rank")["is_attributed"].mean())
