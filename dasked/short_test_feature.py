c34d4b2f758cca28ef5cb4049c83c1427518819c
backonhighway <pocketsuteado@gmail.com>
2018-04-03 12:12:41 +0900

    aha

--- dasked/short_test_feature.py
+++ dasked/short_test_feature.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 from talkingdata.dasked import short_feature_module
 
-TEST_FILE = os.path.join(INPUT_DIR, "merged_test_vanilla.csv")
-OUTPUT_FILE = os.path.join(OUTPUT_DIR, "short_merged_test_vanilla.csv")
+TEST_FILE = os.path.join(INPUT_DIR, "test_old.csv")
+OUTPUT_FILE = os.path.join(OUTPUT_DIR, "short_test_old.csv")
 
 short_feature_module.make_file(TEST_FILE, OUTPUT_FILE)
