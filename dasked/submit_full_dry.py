9702432c6f023c7ea2225e64badd0a6710d2b062
backonhighway <pocketsuteado@gmail.com>
2018-04-03 18:06:51 +0900

    okay

--- dasked/submit_full_dry.py
+++ dasked/submit_full_dry.py
@@ -49,13 +49,14 @@ use_col.remove("is_attributed")
 test = dd.read_csv(TEST_DATA, dtype=dtypes).compute()
 test["is_attributed"] = model.predict(test[use_col], num_iteration=model.best_iteration)
 
+group_cols = ['ip', 'app', 'device', 'os', 'channel', 'click_time']
 join_cols = ['ip', 'app', 'device', 'os', 'channel', 'click_time', "rank"]
 all_cols = join_cols + ['is_attributed']
-test["rank"] = test.groupby(join_cols)["ip"].transform("cumcount")
+test["rank"] = test.groupby(group_cols)["ip"].transform("cumcount")
 print(test.info())
 
 org_test = dd.read_csv(ORG_TEST, dtype=dtypes).compute()
-org_test["rank"] = org_test.groupby(join_cols)["ip"].transform("cumcount")
+org_test["rank"] = org_test.groupby(group_cols)["ip"].transform("cumcount")
 print(org_test.info())
 
 org_test = org_test.merge(test[all_cols], how='left', on=join_cols)
